60e902c22bb500cf15b3ed0296d54d1f484a5a28
vaclav <proficad@gmail.com>
2022-07-25 09:37:20 +0200

    it draws blue circle

--- Viewer_Maui/MainPage.xaml.cs
+++ Viewer_Maui/MainPage.xaml.cs
@@ -1,24 +1,51 @@
-﻿namespace Viewer_Maui;
+﻿using SkiaSharp;
+
+namespace Viewer_Maui;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+
+
+//		skiaView = new SkiaSharp.Views.Maui.Controls.SKCanvasView();
+
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+
+
+	private void Button_About_Clicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		
 	}
+    private void Button_Pages_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+	private void skiaView_PaintSurface(object sender, SkiaSharp.Views.Maui.SKPaintSurfaceEventArgs e)
+	{
+        SKSurface surface = e.Surface;
+        // then we get the canvas that we can draw on
+        SKCanvas canvas = surface.Canvas;
+        // clear the canvas / view
+        canvas.Clear(SKColors.White);
+
+
+        SKPaint paint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SkiaSharp.SKColors.Aqua,
+            StrokeWidth = 25
+        };
+
+        canvas.DrawCircle(100, 100, 20, paint); 
+
+
+    }
 }
 
