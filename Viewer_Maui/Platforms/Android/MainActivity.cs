7b6b4e682a328707300934e46ba9ab8ed4da3ca4
vaclav <proficad@gmail.com>
2022-09-11 18:34:31 +0200

    wired up the view intent to call the VIEW action

--- Viewer_Maui/Platforms/Android/MainActivity.cs
+++ Viewer_Maui/Platforms/Android/MainActivity.cs
@@ -1,10 +1,31 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Content;
 
 namespace Viewer_Maui;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault,Intent.CategoryDefault,
+            Intent.CategoryBrowsable }, DataMimeType = "application/*", DataPathPattern = ".*\\.sxe")]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnNewIntent(Intent? intent)
+    {
+        int o = 5;
+    }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        int u = 5;
+
+        if (Intent.Type == "text/plain" && Intent.Action == "android.intent.action.SEND")
+        {
+            //handleSendUrl();
+            int yy = 88;
+        }
+
+        
+        base.OnCreate(savedInstanceState);
+    }
 }
